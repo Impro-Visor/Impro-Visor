7e562bfd0c3d9a370cc5d39e240090d13b39e8eb
cssummer17 <cssummer17@c26.clinic.cs.hmc.edu>
2017-06-30 14:48:15 -0700

    Converted IntervalLearningFrame into Dialog for better display in Windows.

--- src/imp/gui/IntervalLearningFrame.java
+++ src/imp/gui/IntervalLearningFrame.java
@@ -31,7 +31,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class IntervalLearningFrame
-        extends javax.swing.JFrame
+        extends javax.swing.JDialog
         implements imp.Constants {
 
     
@@ -164,12 +164,12 @@ public class IntervalLearningFrame
 
         generatorWindowMenu1.setLabel("Window");
         generatorWindowMenu1.addMenuListener(new javax.swing.event.MenuListener() {
-            public void menuCanceled(javax.swing.event.MenuEvent evt) {
+            public void menuSelected(javax.swing.event.MenuEvent evt) {
+                generatorWindowMenu1MenuSelected(evt);
             }
             public void menuDeselected(javax.swing.event.MenuEvent evt) {
             }
-            public void menuSelected(javax.swing.event.MenuEvent evt) {
-                generatorWindowMenu1MenuSelected(evt);
+            public void menuCanceled(javax.swing.event.MenuEvent evt) {
             }
         });
 
